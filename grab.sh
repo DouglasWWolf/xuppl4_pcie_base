fc5d5851904387b4dacd32db52d891fb72e270c3
DouglasWWolf <douglaswolf@yahoo.com>
2024-02-21 21:29:07 -0800

    No commit message

---
+++ grab.sh
@@ -0,0 +1,12 @@
+dest=bitstream
+base=project
+
+mkdir $dest 2>/dev/null
+
+
+bit=project.runs/impl_1/top_level_wrapper.bit 
+ltx=project.runs/impl_1/top_level_wrapper.ltx
+
+                cp $bit ${dest}/${base}.bit
+test -f $ltx && cp $ltx ${dest}/${base}.ltx
+
